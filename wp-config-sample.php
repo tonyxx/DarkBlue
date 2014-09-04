ce6b0f5c3e0d8513dc4afd1bc9e4b3cd5e8176c6
dzh1 <dzh1@dzhtekiiMac.local>
2014-09-04 16:45:19 +0800

    fff

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -15,16 +15,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'd2dc3508c6d254ca1a8d203d5edacf730');
+define('DB_NAME', 'database_name_here');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'ubctbWTxwjkAU');
+define('DB_USER', 'username_here');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'pIgqoDFn5fCAQ');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL主机 */
-define('DB_HOST', '10.4.12.46');
+define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
